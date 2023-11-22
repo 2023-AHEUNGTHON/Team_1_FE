f603c7753b76089527fdb2b7888db026936bc3be
dbogym <ko07073@gmail.com>
2023-11-23 02:12:40 +0900

    merge

---
+++ src/components/slu-click.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {  BrowserView ,  MobileView ,  isBrowser ,  isMobile  } from 'react-device-detect' ;
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './solution.css';
+import { Link } from 'react-router-dom';
+
+function slu_click() {
+  return (
+    <div className="slu_click">
+        <h1>asdjadsklkjlasdasddasdasdas</h1>
+    </div>
+  );
+}
+
+export default slu_click;
--- src/components/solution.js
+++ src/components/solution.js
@@ -4,11 +4,14 @@ import {  BrowserView ,  MobileView ,  isBrowser ,  isMobile  } from 'react-devi
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import './solution.css';
+import { Link } from 'react-router-dom';
 
 function solution() {
   return (
     <div className="solution">
-      <Button variant="danger" className='button-slu'>솔루션 받으러 가기</Button>{' '}
+      <Button variant="danger" className='button-slu'>
+        <Link to="/slu_click" className='solution_btn'>솔루션 받으러 가기</Link>
+      </Button>{' '}
     </div>
   );
 }
