675e2e40702ae61141f056315ce3df0692606fbd
sleepy2535 <greenapple5657@gmail.com>
2023-11-25 08:39:40 +0900

    메모

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Solution from "./components/solution";
 import Add_schedule from "./pages/add_schedule";
 import MainSchedule from "./pages/main_schedule";
+import Memo from "./pages/memo";
 import Slu_click from "./components/slu-click";
 import Event from "./components/event";
 import "./App.css";
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/solution" element={<Solution />} />
                     <Route path="/add" element={<Add_schedule />} />
                     <Route path="/main" element={<MainSchedule />} />
+                    <Route path="/memo" element={<Memo />} />
                     <Route path="/slu_click" element={<Slu_click />} />
                     <Route path="/event" element={<Event />} />
                 </Routes>
